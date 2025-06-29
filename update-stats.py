3f24019fb66e4cd2fb4f78579e8d5dcc6197d1bb
andriaK <promopack@proton.me>
2025-06-29 22:52:49 +0400

    Update update-stats.py

--- update-stats.py
+++ update-stats.py
@@ -40,8 +40,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 new_stats = (
     f"<!--START_STATS-->\n"
     f"🧮 Total contributions made during the last 30 days: **`{total}`**  \n"
-    f"📊 Average contributions per day over these 30 days: ## `{average}`  \n"
-    # f"## `{average}`  \n"
+    f"📊 Average contributions per day over these 30 days: **`{average}`**  \n"
     f"🕒 Last updated: **`{timestamp}`**\n"
     f"<!--END_STATS-->"
 )
